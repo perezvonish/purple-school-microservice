3e5dfb0359b3ebaf444b87cc6085b7d0e5127f0c
Vladimir <shokilak@gmail.com>
2023-06-06 20:08:13 +0600

    feat: monorepo 6

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) {}
+
+  @Post("/register")
+  async register(@Body() body: RegisterDto) {
+    return this.authService.register(body)
+  }
+
+  @Post("/login")
+  async login(@Body() body: LoginDto) {
+    const id = await this.authService.validateUser(body)
+
+    return this.authService.login()
+  }
+}
+
+
+export class RegisterDto {
+  email: string
+  password: string
+  displayName?: string
+}
+
+export class LoginDto {
+  email: string
+  password: string
+}
--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {LoginDto, RegisterDto} from "./auth.controller";
+import {UserRepository} from "../user/repositories/user.repository";
+import {UserEntity} from "../user/entities/user.entity";
+import {UserRole} from "@./interfaces";
+import {JwtService} from "@nestjs/jwt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async register({email, password, displayName}: RegisterDto) {
+    const user = await this.userRepository.find(email)
+
+    if (user){
+      throw new Error("User already registered")
+    }
+
+    const userEntity = await new UserEntity({
+      email,
+      displayName,
+      passwordHash: "",
+      role: UserRole.Student
+    }).setPassword(password);
+
+    const newUser = await this.userRepository.create(userEntity)
+
+    return {email: newUser.email}
+  }
+
+  async validateUser({email, password}: LoginDto) {
+    const user = await this.userRepository.find(email)
+    if (!user){
+      throw new Error("User not found")
+    }
+
+    const userEntity = new UserEntity(user)
+
+    const isCorrectPassword = await userEntity.validatePassword(password)
+    if (!isCorrectPassword){
+      throw new Error("Wrong password")
+    }
+
+    return {
+      id: user.id
+    }
+  }
+
+  async login(id: string) {
+    return {
+      access_token: await this.jwtService.signAsync({id})
+    }
+  }
+}
